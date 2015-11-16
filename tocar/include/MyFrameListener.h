fe79a9ac154ae21a385d5663bb1111da48cac655
enriqueforner <enriquefornerdiaz@gmail.com>
2015-11-16 18:54:17 +0100

    tocar

---
+++ tocar/include/MyFrameListener.h
@@ -0,0 +1,32 @@
+#include <Ogre.h>
+#include <OIS.h>
+#include <CEGUI.h>
+
+class MyFrameListener : public Ogre::FrameListener, OIS::KeyListener, OIS::MouseListener {
+private:
+  OIS::InputManager* _inputManager;
+  OIS::Keyboard* _keyboard;
+  OIS::Mouse* _mouse;
+  Ogre::Camera* _camera;
+  Ogre::SceneNode *_node;
+
+  bool keyPressed(const OIS::KeyEvent& evt);
+  bool keyReleased(const OIS::KeyEvent& evt);
+  bool mouseMoved(const OIS::MouseEvent& evt);
+  bool mousePressed(const OIS::MouseEvent& evt, OIS::MouseButtonID id);
+  bool mouseReleased(const OIS::MouseEvent& evt, OIS::MouseButtonID id);
+
+  bool _quit;
+  bool _play;
+  float _timeSinceLastFrame;
+
+  CEGUI::MouseButton convertMouseButton(OIS::MouseButtonID id);
+
+public:
+  MyFrameListener(Ogre::RenderWindow* win, Ogre::Camera* cam, 
+		  Ogre::SceneNode* node);
+  ~MyFrameListener();
+  bool frameStarted(const Ogre::FrameEvent& evt);  
+  bool quit(const CEGUI::EventArgs &e);
+  bool play(const CEGUI::EventArgs &e);
+};
